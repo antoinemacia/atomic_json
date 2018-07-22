cad99226f22bd7d4cbfb35885811688eda701cbf
Antoine Macia <antoine@discolabs.com>
2018-07-22 15:21:40 +1000

    Secure from SQL injection

--- lib/atomic_json/query.rb
+++ lib/atomic_json/query.rb
@@ -3,14 +3,13 @@
 module AtomicJson
   class Query
 
-    CREATE_NEW_COLUMN = false
-
     class QueryError < StandardError; end
 
-    attr_reader :record, :jsonb_field, :create_missing
+    attr_reader :record, :jsonb_field, :create_missing, :connection
     attr_accessor :query
 
     def initialize(record, jsonb_field, create_missing)
+      @connection = ActiveRecord::Base.connection
       @record = record
       @jsonb_field = jsonb_field
       @create_missing = create_missing
@@ -18,18 +17,16 @@ module AtomicJson
 
     def build(attributes)
       self.query = case attributes.keys.count
-      when 0
-        nil
+      when 0 then nil
       when 1
         single_update_query(attributes.keys.first, attributes.values.first)
-      else
-        multi_update_query(attributes)
+      else multi_update_query(attributes)
       end
       self
     end
 
-    def run!
-      ActiveRecord::Base.connection.execute(query)
+    def execute!
+      connection.exec_update(query)
     rescue ActiveRecord::StatementInvalid => e
       raise QueryError, e.message
     end
@@ -38,15 +35,29 @@ module AtomicJson
 
       def single_update_query(key, value)
         <<~SQL
-          UPDATE #{record.class.table_name}
-          SET #{jsonb_field} = jsonb_set(#{jsonb_field}, '{#{key}}', #{type_matcher(value)}, #{create_missing})
-          WHERE id = #{record.id};
+          UPDATE #{connection.quote_table_name(record.class.table_name)}
+          SET #{connection.quote_column_name(jsonb_field)} = jsonb_set(
+                #{connection.quote_column_name(jsonb_field)},
+                #{jsonb_quote_key(key)},
+                #{jsonb_quote_value(value)},
+                #{create_missing}
+              )
+          WHERE id = #{connection.quote(record.id)};
         SQL
       end
 
-      def type_matcher(value)
-        value.is_a?(String) ? "'\"#{value}\"'" : %('#{value}')
+      def jsonb_quote_key(key)
+        case key
+        when String, Symbol then "'{#{key}}'"
+        else raise TypeError
+        end
       end
 
+      def jsonb_quote_value(value)
+        case value
+        when String then "'\"#{connection.quote_string(value)}\"'"
+        else %('#{value}')
+        end
+      end
   end
 end
